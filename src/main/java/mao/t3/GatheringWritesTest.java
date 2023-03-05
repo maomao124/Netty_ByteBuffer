07ae8194200e6bfcd648bfe77242448e5076292a
mao <1296193245@qq.com>
2023-03-05 22:58:24 +0800

    Netty_ByteBuffer

---
+++ src/main/java/mao/t3/GatheringWritesTest.java
@@ -0,0 +1,57 @@
+package mao.t3;
+
+import mao.utils.ByteBufferUtil;
+
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Project name(项目名称)：Netty_ByteBuffer
+ * Package(包名): mao.t3
+ * Class(类名): GatheringWritesTest
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2023/3/5
+ * Time(创建时间)： 22:47
+ * Version(版本): 1.0
+ * Description(描述)： Gathering Writes
+ */
+
+public class GatheringWritesTest
+{
+    public static void main(String[] args)
+    {
+        try (RandomAccessFile randomAccessFile = new RandomAccessFile("data4.txt", "rw"))
+        {
+            FileChannel fileChannel = randomAccessFile.getChannel();
+
+            ByteBuffer byteBuffer1 = ByteBuffer.allocate(4);
+            ByteBuffer byteBuffer2 = ByteBuffer.allocate(3);
+
+            byteBuffer1.put("five".getBytes(StandardCharsets.UTF_8));
+            byteBuffer2.put("six".getBytes(StandardCharsets.UTF_8));
+
+            ByteBuffer[] byteBuffers = new ByteBuffer[]{byteBuffer1, byteBuffer2};
+
+            ByteBufferUtil.debugAll(byteBuffer1);
+            ByteBufferUtil.debugAll(byteBuffer2);
+
+            //切换到读模式
+            byteBuffer1.flip();
+            byteBuffer2.flip();
+
+            ByteBufferUtil.debugAll(byteBuffer1);
+            ByteBufferUtil.debugAll(byteBuffer2);
+
+            //写入
+            fileChannel.write(byteBuffers);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
